8127512a0256c1ce3b1823ed6c5a6202e8c009c6
Kevin Sweeren <sweerenk@gmail.com>
2018-11-23 15:12:03 +0100

    eerste versie

---
+++ src/main/java/com/topicus/CFPApplication/CfpApplication.java
@@ -0,0 +1,12 @@
+package com.topicus.CFPApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfpApplication.class, args);
+	}
+}
